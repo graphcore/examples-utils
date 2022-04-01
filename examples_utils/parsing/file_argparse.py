e7eb13453166794a2bf59646825588baeb74cdef
Blazej Banaszewski <blazejb@graphcore.ai>
2022-04-01 14:59:50 +0100

    refactoring the file argparser

--- examples_utils/parsing/file_argparse.py
+++ examples_utils/parsing/file_argparse.py
@@ -1,39 +1,53 @@
-# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+# Copyright (c) 2022 Graphcore Ltd. All rights reserved.
 
 import yaml
 from typing import List
 from pathlib import Path
 
 
-def _read_yaml_config(config_filename):
-    config_filename = Path(config_filename)
-    with config_filename.open() as config_file:
-        configs = yaml.full_load(config_file)
-    return configs
-
-
 def parse_yaml_config(args, parser):
+    """
+    Overwritting the command and default arguments
+    with values specified in the configuration file.
+    This method allows to verify the correctness of
+    the file arguments based on arguments defined in 
+    the parser.
+
+    args: parsed arguments
+    parser: the parser used to generate "args" to be
+            updated with the yaml config
+    """
+    def _read_yaml_config(config_filename):
+        config_filename = Path(config_filename)
+        with config_filename.open() as config_file:
+            configs = yaml.full_load(config_file)
+        return configs
+
+    def _yaml_to_string_list(config: dict) -> List[str]:
+        s_list = []
+        for arg, value in config.items():
+            s_list += [f'--{arg}']
+            if type(value) == list:
+                s_list += [str(element) for element in value]
+            else:
+                s_list += [str(value)]
+        return s_list
+
     if args.config is not None:
-        # Load the configurations from the YAML file and update command line arguments
+        # Load the configurations from the YAML file 
+        # and update command line arguments
         configs = _read_yaml_config(args.config_path)
         if args.config not in configs:
             raise ValueError(
-                f'unknown config {args.config} in config file. Available configs are {list(configs.keys())}.')
-        string_list_config = _yaml_to_string_list(configs[args.config])
+                f'unknown config {args.config} in config file. '
+                f'Available configs are {list(configs.keys())}.'
+            )
+        string_list_config = _yaml_to_string_list(
+            configs[args.config]
+        )
         config_args = parser.parse_args(string_list_config)
         parser.set_defaults(**vars(config_args))
         cmdline_args = parser.parse_args()
         args = cmdline_args
 
     return args
-
-
-def _yaml_to_string_list(config: dict) -> List[str]:
-    s_list = []
-    for arg, value in config.items():
-        s_list += [f'--{arg}']
-        if type(value) == list:
-            s_list += [str(element) for element in value]
-        else:
-            s_list += [str(value)]
-    return s_list
