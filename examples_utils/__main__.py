1a4ef42bfeb31ad1137db1c3c32fd21a6871bf72
hiteshk <hiteshk@graphcore.ai>
2022-08-23 14:29:42 +0100

    Revert "Bump cppimport to `>=22.07.17` (#23)"
    This reverts commit 8a7cfdb33c28261a1921bd404e4b42675d13393c.

--- examples_utils/__main__.py
+++ examples_utils/__main__.py
@@ -17,6 +17,11 @@ def main(raw_args):
         'load_lib_build', description='Use load_lib to build all eligible files in specified directory.')
     load_lib_build_parser(load_lib_build_subparser)
 
+    # Cppimport CLI backported. Can be removed once version is upgraded
+    cppimport_build_subparser = subparsers.add_parser(
+        'cppimport_build', description='Backported from cppimport. Equivalent to `python3 -m cppimport build`')
+    _run_from_commandline_argparse(cppimport_build_subparser)
+
     benchmarks_subparser = subparsers.add_parser(
         'benchmark', description="Run applications benchmarks from the application's root directory.")
     benchmarks_parser(benchmarks_subparser)
@@ -32,13 +37,15 @@ def main(raw_args):
 
     if args.subparser == 'load_lib_build':
         load_lib_builder_run(args)
+    elif args.subparser == 'cppimport_build':
+        _run_from_commandline_run(args)
     elif args.subparser == 'benchmark':
         configure_logger(args)
         run_benchmarks(args)
     elif args.subparser == 'test_copyright':
         test_copyrights(args.path, args.amend, args.exclude_json)
     else:
-        raise Exception('Please select: `load_lib_build` or `benchmark`')
+        raise Exception('Please select: `load_lib_build`, `cppimport_build` or `benchmark`')
 
 
 if __name__ == "__main__":
