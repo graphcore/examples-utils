32fbcfa868f0f1aa652cff3cc4f8d02920d50b16
hiteshk-gc <hiteshk@graphcore.ai>
2023-02-26 17:22:01 +0000

    linting

--- examples_utils/command_logger/gc_logger.py
+++ examples_utils/command_logger/gc_logger.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2022 Graphcore Ltd. All rights reserved.
 
+import ipynbname
 import json
 import os
 import pathlib
+import pkg_resources
 import psutil
 import shlex
 import subprocess
@@ -15,7 +17,11 @@ class GCLogger(object):
     GC_LOG_CFG_PATH = pathlib.Path.home().joinpath(".graphcore", "logs", f"{time.strftime('%Y_%m_%d')}")
     GC_LOG_CFG_FILE = GC_LOG_CFG_PATH.joinpath("config.json")
     GC_LOG_STATE = None
-    GC_LOG_DELAY_SECONDS = 10
+
+    FREQUENT_POLLING_SECONDS = 10
+    INFREQUENT_POLLING_SECONDS = 60
+
+    metrics_dict = {}
 
     def __new__(cls):
         if cls._instance is None:
@@ -60,21 +66,10 @@ class GCLogger(object):
         return cls._instance
 
     @classmethod
-    def __write_dict_to_file(cls, log_dict, filename):
-        if cls.GC_LOG_STATE == "DISABLED":
-            return
-
+    def __write_json(cls, dict, filename, style="w"):
         try:
-            if not filename.is_file():
-                filename.parent.mkdir(parents=True, exist_ok=True)
-                with open(filename, "w") as f:
-                    json.dump({"log": []}, f)
-
-            with open(filename, "r+") as f:
-                conf = json.load(f)
-                conf["log"].append(log_dict)
-                f.seek(0)
-                json.dump(conf, f)
+            with open(cls.GC_LOG_CFG_PATH.joinpath(f"{filename}.json"), style) as outfile:
+                json.dump(dict, outfile)
         except Exception as e:
             sys.stderr.write(f"Config logging error logging to file: {e}")
 
@@ -101,27 +96,6 @@ class GCLogger(object):
             subprocess.run(command, stdout=outfile, stderr=outfile)
 
     @classmethod
-    def __log_sysperf_metrics(cls):
-        if cls.GC_LOG_STATE == "DISABLED":
-            return
-
-        while True:
-            # CPU utilisation
-            cls.iteration_dict["cpu_percent"] = psutil.cpu_percent()
-
-            # virtual/swap memory usage
-            cls.iteration_dict["virtual_memory"] = psutil.virtual_memory().percent
-            cls.iteration_dict["swap_memory"] = psutil.swap_memory().percent
-
-            # Disk usage
-            cls.iteration_dict["disk_used"] = psutil.disk_usage("/").used
-
-            with open(cls.GC_LOG_CFG_PATH.joinpath("cpu_metrics.log"), "a") as outfile:
-                json.dump(cls.iteration_dict, outfile)
-
-            time.sleep(cls.GC_LOG_DELAY_SECONDS)
-
-    @classmethod
     def __log_ipuperf_info(cls):
         if cls.GC_LOG_STATE == "DISABLED":
             return
@@ -142,19 +116,111 @@ class GCLogger(object):
             subprocess.run(shlex.split("gc-iputraffictest --all-links -j"), stdout=outfile, stderr=outfile)
 
     @classmethod
+    def __log_file_info(cls):
+        if cls.GC_LOG_STATE == "DISABLED":
+            return
+
+        # Notebook path
+        cls.notebook_dict = {
+            "notebook_path": ipynbname.path(),
+        }
+
+        cls.__write_json(cls, cls.notebook_dict, "notebook_info")
+
+    @classmethod
+    def __log_dataset_info(cls):
+        if cls.GC_LOG_STATE == "DISABLED":
+            return
+
+        # Overwrite old info - likely datasets persist throughout and probably
+        # will only increase in size
+        while True:
+            # How to find out which datasets are being used?
+
+            # Find location/path
+
+            # Find size
+
+            time.sleep(cls.INFREQUENT_POLLING_SECONDS)
+
+    @classmethod
+    def __log_sysperf_metrics(cls):
+        if cls.GC_LOG_STATE == "DISABLED":
+            return
+
+        while True:
+            iteration_dict = {}
+
+            # CPU utilisation
+            iteration_dict["cpu_percent"] = psutil.cpu_percent()
+
+            # virtual/swap memory usage
+            iteration_dict["virtual_memory"] = psutil.virtual_memory().percent
+            iteration_dict["swap_memory"] = psutil.swap_memory().percent
+
+            # Disk usage
+            iteration_dict["disk_used"] = psutil.disk_usage("/").used
+
+            cls.metrics_dict[time.time()] = iteration_dict
+
+            time.sleep(cls.FREQUENT_POLLING_SECONDS)
+
+    @classmethod
+    def __log_file_metrics(cls):
+        if cls.GC_LOG_STATE == "DISABLED":
+            return
+
+        search_dir = pathlib.Path(cls.notebook_dict["notebook_path"]).parents[1]
+
+        while True:
+            # Get all .popef files name and size
+            popef_files = search_dir.glob("*.popef")
+
+            popef_dict = {}
+            for file in popef_files:
+                popef_dict.update({str(file): file.stat().st_size})
+
+            cls.__write_json(cls, popef_dict, "popef_files")
+
+            # Search for all weight files and poll size/name
+            weights_extensions = ["onnx", "pt", "pb"]
+            weight_files = []
+            for ext in weights_extensions:
+                weight_files.append(search_dir.glob(f"*.{ext}"))
+
+            weight_dict = {}
+            for file in weight_files:
+                weight_dict.update({str(file): file.stat().st_size})
+
+            cls.__write_json(cls, weight_dict, "weight_files")
+
+            # Query pip packages and versions
+            pkgs_dict = {i.key: i.version for i in pkg_resources.working_set}
+            cls.__write_json(cls, pkgs_dict, "python_packages")
+
+            time.sleep(cls.INFREQUENT_POLLING_SECONDS)
+
+    @classmethod
+    def __log_notebook_usage(cls):
+        pass
+
+    @classmethod
     def start_logging(cls):
         if cls.GC_LOG_STATE == "DISABLED":
             return
 
-        iteration_dict = {}
-
+        # One-time collection (constant, static information on system/env)
         cls.__log_sysperf_info(cls)
+        cls.__log_ipuperf_info(cls)
+        cls.__log_file_info(cls)
+        cls.__log_dataset_info(cls)
 
+        # Frequent polling (changing values, metrics, measurements on system/env)
         cls.__log_sysperf_metrics(cls)
 
-        cls.__log_ipuperf_info(cls)
-
-        cls.log_dict[str(time.time())] = iteration_dict
+        # Infrequent polling (names, file sizes, packages etc.)
+        cls.__log_file_metrics(cls)
+        cls.__log_notebook_usage(cls)
 
 
 GCLogger()
