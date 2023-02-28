bcae7b4773c4a48d811a1662deb40cdbe89d7a16
hiteshk-gc <hiteshk@graphcore.ai>
2023-02-28 10:15:47 +0000

    Adding compile-time tracking

--- examples_utils/command_logger/gc_logger.py
+++ examples_utils/command_logger/gc_logger.py
@@ -2,6 +2,7 @@
 
 import ipynbname
 import json
+import nbformat
 import os
 import pathlib
 import pkg_resources
@@ -18,8 +19,8 @@ class GCLogger(object):
     GC_LOG_CFG_FILE = GC_LOG_CFG_PATH.joinpath("config.json")
     GC_LOG_STATE = None
 
-    FREQUENT_POLLING_SECONDS = 10
-    INFREQUENT_POLLING_SECONDS = 60
+    FAST_POLLING_SECONDS = 10
+    SLOW_POLLING_SECONDS = 60
 
     metrics_dict = {}
 
@@ -101,7 +102,7 @@ class GCLogger(object):
             return
 
         # Get information for each IPU available
-        with open(cls.GC_LOG_CFG_PATH.joinpath("ipu_perf.log"), "a") as outfile:
+        with open(cls.GC_LOG_CFG_PATH.joinpath("ipu_perf.json"), "a") as outfile:
             num_ipus = os.getenv("NUM_AVAILABLE_IPU")
 
             # Host <-> IPU sync latency
@@ -132,25 +133,15 @@ class GCLogger(object):
         if cls.GC_LOG_STATE == "DISABLED":
             return
 
-        search_dir = pathlib.Path("/")
-
         # Overwrite old info - likely datasets persist throughout and probably
         # will only increase in size
         while True:
             # Look for everything that looks like a dataset
-            data_dirs = search_dir.glob("*/*/*dataset*")
-
-            popef_dict = {}
-            for file in popef_files:
-                popef_dict.update({str(file): file.stat().st_size})
-
-            cls.__write_json(cls, popef_dict, "popef_files")
-
             # Find location/path
 
             # Find size
 
-            time.sleep(cls.INFREQUENT_POLLING_SECONDS)
+            time.sleep(cls.SLOW_POLLING_SECONDS)
 
     @classmethod
     def __log_sysperf_metrics(cls):
@@ -172,34 +163,32 @@ class GCLogger(object):
 
             cls.metrics_dict[time.time()] = iteration_dict
 
-            time.sleep(cls.FREQUENT_POLLING_SECONDS)
+            time.sleep(cls.FAST_POLLING_SECONDS)
 
     @classmethod
     def __log_file_metrics(cls):
         if cls.GC_LOG_STATE == "DISABLED":
             return
 
-        search_dir = pathlib.Path(cls.notebook_dict["notebook_path"]).parents[1]
-
         while True:
             # Get all .popef files name and size
-            popef_files = search_dir.glob("*.popef")
-
+            popef_files = pathlib.Path("/tmp/exe_cache").glob("**/*.popef")
             popef_dict = {}
             for file in popef_files:
-                popef_dict.update({str(file): file.stat().st_size})
+                popef_dict[str(file)] = file.stat().st_size
 
             cls.__write_json(cls, popef_dict, "popef_files")
 
             # Search for all weight files and poll size/name
             weights_extensions = ["onnx", "pt", "pb"]
             weight_files = []
+            search_dir = pathlib.Path(cls.notebook_dict["notebook_path"]).parents[1]
             for ext in weights_extensions:
-                weight_files.append(search_dir.glob(f"*.{ext}"))
+                weight_files.append(search_dir.glob(f"**/*.{ext}"))
 
             weight_dict = {}
             for file in weight_files:
-                weight_dict.update({str(file): file.stat().st_size})
+                weight_dict[str(file)] = file.stat().st_size
 
             cls.__write_json(cls, weight_dict, "weight_files")
 
@@ -207,29 +196,77 @@ class GCLogger(object):
             pkgs_dict = {i.key: i.version for i in pkg_resources.working_set}
             cls.__write_json(cls, pkgs_dict, "python_packages")
 
-            time.sleep(cls.INFREQUENT_POLLING_SECONDS)
+            time.sleep(cls.SLOW_POLLING_SECONDS)
 
     @classmethod
     def __log_notebook_usage(cls):
-        pass
+        if cls.GC_LOG_STATE == "DISABLED":
+            return
+
+        while True:
+            relevant_outputs = {}
+
+            with open(pathlib.Path(cls.notebook_dict["notebook_path"])) as notebook:
+                raw_notebook = nbformat.read(notebook, nbformat.NO_CONVERT)
+
+            # Get all code cells, search for compile time
+            code_cell_outputs = [cell["outputs"] for cell in raw_notebook["cells"] if cell["cell_type"] == "code"]
+            for output in code_cell_outputs:
+                # Some cells have a seperate 'data' outputs. We need 'text' output
+                if len(output) > 1:
+                    output = output[1]
+
+                text = output.get("text")
+                if text is not None and "compil" in text:  # "compil" here is purposeful
+                    relevant_outputs[""]
+
+            time.sleep(cls.SLOW_POLLING_SECONDS)
+
+    @classmethod
+    def __log_compile_times(cls):
+        if cls.GC_LOG_STATE == "DISABLED":
+            return
+
+        while True:
+            relevant_outputs = {}
+
+            with open(pathlib.Path(cls.notebook_dict["notebook_path"])) as notebook:
+                raw_notebook = nbformat.read(notebook, nbformat.NO_CONVERT)
+
+            # Get all code cells, search for compile time
+            code_cell_outputs = [cell["outputs"] for cell in raw_notebook["cells"] if cell["cell_type"] == "code"]
+            for output in code_cell_outputs:
+                # Some cells have a seperate 'data' outputs. We need 'text' output
+                if len(output) > 1:
+                    output = output[1]
+
+                text = output.get("text")
+                if text is not None and "compil" in text:  # "compil" here is purposeful
+                    relevant_outputs[""]
+
+            time.sleep(cls.SLOW_POLLING_SECONDS)
 
     @classmethod
     def start_logging(cls):
         if cls.GC_LOG_STATE == "DISABLED":
             return
 
-        # One-time collection (constant, static information on system/env)
+        # One-time collection
+        # (constant, static information on system/env)
         cls.__log_sysperf_info(cls)
         cls.__log_ipuperf_info(cls)
         cls.__log_file_info(cls)
         cls.__log_dataset_info(cls)
 
-        # Frequent polling (changing values, metrics, measurements on system/env)
+        # Frequent polling every cls.FAST_POLLING_SECONDS
+        # (changing values, metrics, measurements on system/env)
         cls.__log_sysperf_metrics(cls)
 
-        # Infrequent polling (names, file sizes, packages etc.)
+        # Infrequent polling every cls.SLOW_POLLING_SECONDS
+        # (names, file sizes, packages etc.)
         cls.__log_file_metrics(cls)
         cls.__log_notebook_usage(cls)
+        cls.__log_compile_times(cls)
 
 
 GCLogger()
