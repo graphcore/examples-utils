2563e91ee507d1e948a215799921a93954f6d538
hiteshk-gc <hiteshk@graphcore.ai>
2023-03-31 16:16:48 +0000

    Adding exeception handling around file move

--- examples_utils/notebook_logging/__init__.py
+++ examples_utils/notebook_logging/__init__.py
@@ -6,7 +6,11 @@ from pathlib import Path
 from .gc_logger import *
 
 # Help IPython find our custom extension
-extension_path = Path(__file__).parent.joinpath("gc_logger.py").resolve()
-destination_path = Path("/root/.ipython/extensions").resolve()
+try:
+    extension_path = Path(__file__).parent.joinpath("gc_logger.py").resolve()
+    destination_path = Path("/root/.ipython/extensions").resolve()
 
-subprocess.run(f"cp {extension_path} {destination_path}", shell=True)
+    subprocess.run(f"cp {extension_path} {destination_path}", shell=True)
+# No logs to user if error
+except:
+    pass
