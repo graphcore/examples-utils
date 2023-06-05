674183ae2eaefee050ba970c5c4db521f4a2f41a
Rahul Trada <rahult@graphcore.ai>
2023-06-05 16:04:23 +0100

    debugprint

--- examples_utils/benchmarks/notebook_utils.py
+++ examples_utils/benchmarks/notebook_utils.py
@@ -33,6 +33,7 @@ def run_notebook(notebook_filename: str, working_directory: str, timeout: int = 
     try:
         ep.preprocess(nb, {"metadata": {"path": f"{working_directory}"}})
     except CellExecutionError:
+        print("RRR cell execution error")
         output, _ = exporter.from_notebook_node(nb)
         print(output)
         raise
