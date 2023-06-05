7a7e6f50a365b2c1c6195d97505f9246dcf4a5f4
Rahul Trada <rahult@graphcore.ai>
2023-06-05 12:39:04 +0100

    lower timeout

--- examples_utils/benchmarks/notebook_utils.py
+++ examples_utils/benchmarks/notebook_utils.py
@@ -13,7 +13,7 @@ except (ImportError, ModuleNotFoundError) as error:
 
     raise _incorrect_requirement_variant_error from error
 
-DEFAULT_TIMEOUT = 600
+DEFAULT_TIMEOUT = 5
 
 
 def run_notebook(notebook_filename: str, working_directory: str, timeout: int = DEFAULT_TIMEOUT) -> str:
@@ -27,6 +27,7 @@ def run_notebook(notebook_filename: str, working_directory: str, timeout: int = 
 
     with open(notebook_filename) as f:
         nb = nbformat.read(f, as_version=4)
+    print("RRR timeout: ", timeout)
     ep = ExecutePreprocessor(timeout=timeout, kernel_name="python3")
     exporter = OutputExporter()
     try:
