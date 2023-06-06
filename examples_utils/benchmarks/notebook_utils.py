e67f7f53f188152ff0e3e8241918c0c51d9e47b7
Rahul Trada <rahult@graphcore.ai>
2023-06-06 10:22:18 +0100

    debug

--- examples_utils/benchmarks/notebook_utils.py
+++ examples_utils/benchmarks/notebook_utils.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2022 Graphcore Ltd. All rights reserved.
 import argparse
 import os
+import subprocess
 
 try:
     import nbformat
@@ -29,6 +30,8 @@ def run_notebook(notebook_filename: str, working_directory: str, timeout: int = 
         nb = nbformat.read(f, as_version=4)
     print("RRR timeout: ", timeout)
     print('RRR env: ', os.environ)
+    piplist = subprocess.check_output(['pip', 'list']).decode('utf-8')
+    print('RRR pip list: ', piplist)
     ep = ExecutePreprocessor(timeout=timeout, kernel_name="python3")
     exporter = OutputExporter()
     try:
